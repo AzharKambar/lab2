0e2dd7f0f5ea116915bf35e334fc415dd7781ef2
AzharKambar <124907708+AzharKambar@users.noreply.github.com>
2023-10-03 21:57:48 +0300

    lab2_task3_Azhar.py

--- lab2_task3_Azhar.py
+++ lab2_task3_Azhar.py
@@ -1,9 +1,12 @@
-#вводим три числа
-num1=int(input())
-num2=int(input())
-num3=int(input())
-#условие арифметической прогрессии
-if num2-num1==num3-num2:
-    print('YES')
-else:
-    print('NO')
+try:
+    #вводим три числа
+    num1=int(input())
+    num2=int(input())
+    num3=int(input())
+    #условие арифметической прогрессии
+    if num2-num1==num3-num2:
+        print('YES')
+    else:
+        print('NO')
+except ValueError:
+    print("Input is not correct")
