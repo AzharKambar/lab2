3d8d9dd549b24b0fa002341f9353d25047ba9761
AzharKambar <124907708+AzharKambar@users.noreply.github.com>
2023-10-03 21:56:19 +0300

    lab2_task2_Azhar.py

--- lab2_task2_Azhar.py
+++ lab2_task2_Azhar.py
@@ -1,7 +1,10 @@
-#вводим возраст пользователя
-age=int(input("Введите ваш возраст: "))
-#с помощью условия проверяем если возраст больше равно 18
-if age>=18 :
-    print("Access is allowed")
-else:
-    print("Access denied")
+try:
+    #вводим возраст пользователя
+    age=int(input("Введите ваш возраст: "))
+    #с помощью условия проверяем если возраст больше равно 18
+    if age>=18 :
+        print("Access is allowed")
+    else:
+        print("Access denied")
+except ValueError:
+    print("Input is not correct")
